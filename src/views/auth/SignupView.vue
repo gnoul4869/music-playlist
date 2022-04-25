156bc276e2710def0425378513389952bb90d723
gnoul_ <ttluong4869@gmail.com>
2022-04-25 18:22:34 +0700

    Create SignupView

--- src/views/auth/SignupView.vue
+++ src/views/auth/SignupView.vue
@@ -1,28 +1,30 @@
 <template>
     <form @submit.prevent="handleSubmit">
-        <h3>Log in</h3>
+        <h3>Sign up</h3>
+        <input type="text" placeholder="Display name" v-model="displayName" />
         <input type="email" placeholder="Email" v-model="email" />
         <input type="password" placeholder="Password" v-model="password" />
         <div v-if="error" class="error">{{ error }}</div>
-        <button v-if="!isPending">Log in</button>
-        <button v-if="isPending" disabled>Logging in...</button>
+        <button v-if="!isPending">Sign up</button>
+        <button v-if="isPending" disabled>Signing up...</button>
     </form>
 </template>
 
 <script setup>
 import { ref } from '@vue/reactivity';
-import useLogin from '@/composables/useLogin';
+import useSignup from '@/composables/useSignup';
 
-const { error, isPending, login } = useLogin();
+const { error, isPending, signup } = useSignup();
 
+const displayName = ref('');
 const email = ref('');
 const password = ref('');
 
 const handleSubmit = async () => {
-    const res = await login(email.value, password.value);
+    const res = await signup(email.value, password.value, displayName.value);
 
     if (!error.value) {
-        console.log('user logged in');
+        console.log('user signed up');
     }
 };
 </script>
