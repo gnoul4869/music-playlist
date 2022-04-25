466811f5d6784a10f80d569d964f7a0ddd6ef271
gnoul_ <ttluong4869@gmail.com>
2022-04-25 11:33:06 +0700

    Remove auto generated files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <main class="home">
+        <p>Homepage</p>
+    </main>
 </template>
