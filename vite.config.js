58b4742b0f488d6ae5149d21752fcf5b43fb57f5
gnoul_ <ttluong4869@gmail.com>
2022-04-28 10:00:44 +0700

    Install vite-plugin-rewrite-all package to fix dots in path in dev environment

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from 'url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// Fix dots in path in development enviroment
+import pluginReWriteAll from 'vite-plugin-rewrite-all';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    plugins: [vue(), pluginReWriteAll()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+});
